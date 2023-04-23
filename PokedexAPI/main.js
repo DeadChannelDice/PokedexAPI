629e6182e1041b97002d93a0b80e6850352b7d66
DeadChannelDice <michael.charpin@gmail.com>
2023-04-23 18:43:18 -0400

    Got the API running and updating Notion

--- PokedexAPI/main.js
+++ PokedexAPI/main.js
@@ -4,36 +4,81 @@ const axios = require('axios');
 
 const { Client } = require("@notionhq/client")
 
-const notion = new Client({
-  auth: process.env.NOTION_TOKEN,
-})
+const notion = new Client({ auth: process.env.NOTION_TOKEN})
 
 const pokeArray = [];
 
 async function getPokemon() {
-  await axios.get('https://pokeapi.co/api/v2/pokemon/2')
-  .then((poke) => {
-    
-    const pokeData = {
-      "name": poke.data.species.name,
-      "number": poke.data.id,
-      "hp": poke.data.stats[0].base_stat,
-      "height": poke.data.height,
-      "weight": poke.data.weight,
-      "attack": poke.data.stats[1].base_stat,
-      "defense": poke.data.stats[2].base_stat,
-      "special-attack": poke.data.stats[3].base_stat,
-      "special-defense": poke.data.stats[4].base_stat,
-      "speed": poke.data.stats[5].base_stat,
-    }
 
-    pokeArray.push(pokeData)
-    console.log(`Fetching ${pokeData.name} from PokeAPI`);
-  })
-  .catch ((error) => {
-    console.log(error);
-  })
+  for (let  i = 26; i <= 500; i++) {
+    await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    .then((poke) => {
+      
+      const typesArray = []
 
+      for (let type of poke.data.types) {
+        const typeObj = {
+          "name": type.type.name 
+        }
+        typesArray.push(typeObj)
+      }
+
+      const processedName = poke.data.species.name.split(/-/).map((name) => {
+        return name[0].toUpperCase() + name.substring(1);
+      }).join(" ")
+      .replace(/^Mr M/, "Mr. M")
+
+     
+
+      const bulbURL =  `https://bulbapedia.bulbagarden.net/wiki/${processedName.replace(" ", "_")}_(Pokémon)`
+
+     
+
+      const sprite = (!poke.data.sprites.front_default) ? 
+        poke.data.sprites.other['official-artwork'].front_default : 
+        poke.data.sprites.front_default
+
+      const pokeData = {
+        "name": processedName,
+        "number": poke.data.id,
+        "types": typesArray,
+        "hp": poke.data.stats[0].base_stat,
+        "height": poke.data.height,
+        "weight": poke.data.weight,
+        "attack": poke.data.stats[1].base_stat,
+        "defense": poke.data.stats[2].base_stat,
+        "special-attack": poke.data.stats[3].base_stat,
+        "special-defense": poke.data.stats[4].base_stat,
+        "speed": poke.data.stats[5].base_stat,
+        "sprite": sprite,
+        "artwork": poke.data.sprites.other['official-artwork'].front_default,
+        "bulbURL": bulbURL,
+      } 
+  
+      pokeArray.push(pokeData)
+      console.log(`Fetching ${pokeData.name} from PokeAPI`);
+    })
+    .catch ((error) => {
+      console.log(error);
+    })
+  }
+  
+  for (let pokemon of pokeArray) {
+    const flavor = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.number}`)
+    .then((flavor) => {
+        
+      const flavorText = flavor.data.flavor_text_entries.find(({language: { name }}) => name ==="en").flavor_text.replace(/\n|\r|\f/g, " ")
+      pokemon['flavor-text'] = flavorText
+
+      const category = flavor.data.genera.find(({language: { name }}) => name ==="en").genus
+      pokemon.category = category
+
+      const generation = flavor.data.generation.name.split(/-/).pop().toUpperCase()
+      pokemon.generation = generation
+
+      console.log(`Fetches flavor info for ${pokemon.name}`)
+    })
+}
   createNotionPage()
 }
 
@@ -44,14 +89,25 @@ async function createNotionPage () {
     console.log('Sending Data to Notion')
     const response = await notion.pages.create({
       "parent": {
-        "type": 'database_id',
+        "type": "database_id",
         "database_id": process.env.NOTION_DATABASE_ID
+      }, 
+      "icon": {
+        "type": "external",
+        "external": {
+          "url": pokemon.sprite
+        }
+      },
+      "cover": {
+        "type": "external",
+        "external": {
+          "url": pokemon.artwork
+        }
       },
       "properties": {
         "Name": {
           "title": [
             {
-              "type": "text",
               "text": {
                 "content": pokemon.name
               }
@@ -61,6 +117,22 @@ async function createNotionPage () {
         "No": {
           "number": pokemon.number
         },
+        "Type": {"multi_select": pokemon.types},
+        "Generation": { 
+          "select": {
+            "name": pokemon.generation
+          }
+        },
+        "Category": {
+          "rich_text": [
+            {
+              "type": "text",
+              "text": {
+                "content": pokemon.category
+              }
+            }
+          ]
+        },
         "HP": { "number": pokemon.hp},
         "Attack": { "number": pokemon.attack},
         "Defense": { "number": pokemon.defense},
@@ -68,9 +140,42 @@ async function createNotionPage () {
         "Sp. Defense": { "number": pokemon['special-defense']},
         "Speed": { "number": pokemon.speed},
         "Height": { "number": pokemon.height},
-        "Weight": { "number": pokemon.weight},
+        "Weight": { "number": pokemon.weight}
         
-      } 
+      },
+      "children": [
+        {
+          "object": "block",
+          "type": "quote",
+          "quote": {
+            "rich_text": [{
+              "type": "text",
+              "text": {
+                "content": pokemon['flavor-text']
+              }
+            }]
+          }
+        },
+        {
+          "object": "block",
+          "type": "paragraph",
+          "paragraph": {
+            "rich_text": [{
+              "type": "text",
+              "text": {
+                "content": ""
+              }
+            }]
+          }
+        },
+        {
+          "object": "block",
+          "type": "bookmark",
+          "bookmark": {
+            "url":pokemon.bulbURL
+          }
+        }
+      ]
     })
     console.log(response)
   }
